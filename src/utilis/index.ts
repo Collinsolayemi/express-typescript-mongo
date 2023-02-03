4c6ed321d9bec7db2c92b505cc652b416dee0a02
collins <collinsolayemi@gmail.com>
2023-02-02 19:02:59 -0800

    project setup

---
+++ src/utilis/index.ts
@@ -0,0 +1,6 @@
+import express, { Application } from 'express';
+import mongoose from 'mongoose';
+import compression from 'compression';
+import cors from 'cors';
+import morgan from 'morgan';
+import Controller from '@utilis/interfaces/controller.interface';
