00f9865256809293aba195299980172ba86b9080
collins <collinsolayemi@gmail.com>
2023-02-04 16:37:10 -0800

    setting up dependencies

--- src/utilis/index.ts
+++ src/utilis/index.ts
@@ -4,3 +4,33 @@ import compression from 'compression';
 import cors from 'cors';
 import morgan from 'morgan';
 import Controller from '@utilis/interfaces/controller.interface';
+import ErrorMiddleware from '@middleware/error.middleware';
+import helmet from 'helmet';
+
+class App {
+    public express: Application;
+    public port: number;
+
+    constructor(controllers: Controller[], port: number) {
+        this.express = express();
+        this.port = port;
+
+        this.initialiseDatabaseConnection();
+        this.initialiseMiddleware();
+        this.initialiseControllers(controllers);
+        this.initialiseErrorHandling();
+    }
+    private initialiseMiddleware(): void {
+        this.express.use(helmet());
+        this.express.use(cors());
+        this.express.use(morgan('dev'));
+        this.express.use(express.json());
+        this.express.use(express.urlencoded({ extended: false }));
+        this.express.use(compression());
+    }
+    private initialiseControllers(controllers: controller[]): void {
+        controllers.forEach((controller: controller) => {
+            this.express.use('/api', controller.router());
+        });
+    }
+}
