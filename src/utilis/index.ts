4f84dde57226651d6a5bc1e6da9837b03b92f82d
collins <collinsolayemi@gmail.com>
2023-02-07 09:14:29 -0800

    added more functionalities

--- src/utilis/index.ts
+++ src/utilis/index.ts
@@ -1,51 +1,10 @@
-import express, { Application } from 'express';
-import mongoose from 'mongoose';
-import compression from 'compression';
-import cors from 'cors';
-import morgan from 'morgan';
-import Controller from '@utilis/interfaces/controllers.interface';
-import ErrorMiddleware from '@middleware/error.middleware';
-import helmet from 'helmet';
-import dotenv from 'dotenv';
-dotenv.config();
+import 'dotenv/config';
+import 'module-alias/register';
+import validateEnv from '@utilis/validateEnv';
+import App from './app';
 
-class App {
-    public express: Application;
-    public port: number;
+validateEnv();
 
-    constructor(controllers: Controller[], port: number) {
-        this.express = express();
-        this.port = port;
+const app = new App([], Number(process.env.PORT));
 
-        this.initialiseDatabaseConnection();
-        this.initialiseMiddleware();
-        this.initialiseControllers(controllers);
-        this.initialiseErrorHandling();
-    }
-    private initialiseMiddleware(): void {
-        this.express.use(helmet());
-        this.express.use(cors());
-        this.express.use(morgan('dev'));
-        this.express.use(express.json());
-        this.express.use(express.urlencoded({ extended: false }));
-        this.express.use(compression());
-    }
-    private initialiseControllers(controllers: Controller[]): void {
-        controllers.forEach((controller: Controller) => {
-            this.express.use('/api', controller.router);
-        });
-    }
-    private initialiseErrorHandling(): void {
-        this.express.use(ErrorMiddleware);
-    }
-    private initialiseDatabaseConnection(): void {
-        const { MONGO_URI } = process.env;
-        mongoose.connect(`${MONGO_URI}`);
-    }
-    public listen(): void {
-        this.express.listen(this.port);
-        console.log(`Apps running on port ${this.port}`);
-    }
-}
-
-export default App;
+app.listen();
